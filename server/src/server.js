4ce1f25a48e4672b9c4e7dcba3a4a12530ff014a
Carmelo Campione <cmcampione@gmail.com>
2017-11-14 12:59:00 +0100

    realtime module definition

--- server/src/server.js
+++ server/src/server.js
@@ -14,6 +14,7 @@ const mongoose  		= require("mongoose");
 
 const utils				= require("./utils");
 const usersManager		= require("./bl/usersMngr");
+const clientsConnectorsManager = require("./clientsConnectorsManager");
 
 // Env configuration
 
@@ -134,76 +135,4 @@ httpsServer.listen(port, (err) => {
 
 // Realtime communication support
 
-// SignalR, Socket.io, Internal, ...
-class IClientsConnector {
-	// Typically have a list of specific connections
-}
-
-// Socket.io support
-class ClientsConnectorSocketIO extends IClientsConnector {
-	constructor(server) {
-		super();
-
-		let self = this;
-		
-		this.connections = new Map();
-
-		this.io = require("socket.io")(server);
-
-		this.io.use(async (socket, next) => {
-			let token = socket.handshake.query.token;
-			if (!token)
-				return next(new utils.ErrorCustom(httpStatus.UNAUTHORIZED, httpStatus.getStatusText(httpStatus.UNAUTHORIZED), 11));
-
-			let user = await usersManager.findUserByMasterApiKey(token);
-			if (!user)
-				return next(new utils.ErrorCustom(httpStatus.UNAUTHORIZED, httpStatus.getStatusText(httpStatus.UNAUTHORIZED), 12));
-
-			let userId = user._id.toString();
-			let userSockets = self.connections.get(userId);
-			if (!userSockets) {
-				self.connections.set(userId, new Array());
-				userSockets = self.connections.get(userId);
-			}
-			userSockets.push(socket);
-
-			return next();
-		});
-		
-		this.io.on("connection", function (socket) {
-
-			socket.on("disconnect", (reason) => {
-
-				var mapIter = self.connections.entries();
-				for(let connection of mapIter) {
-					let userId 		= connection[0];
-					let userSockets = connection[1];
-					let socketToRemove = userSockets.find(s => { return s.id == socket.id; });
-					if (!socketToRemove) {
-						continue;
-					}
-					let i = userSockets.indexOf(socketToRemove);
-					userSockets.splice(i, 1);
-					if (userSockets.length == 0)
-						self.connections.delete(userId);
-					break;
-				}
-			});
-			
-			socket.emit("news", { hello: "world" });
-			socket.on("my other event", function (data) {
-				console.log(data);
-			});
-		});
-	}
-}
-
-class ClientsConnectorsManager {
-	static initialize(server) {
-		ClientsConnectorsManager.ClientsConnectors = []; // List of ClientsConnectors
-
-		ClientsConnectorsManager.ClientsConnectors.push(new ClientsConnectorSocketIO(server));
-	}
-}
-ClientsConnectorsManager.initialize(httpsServer);
-
+clientsConnectorsManager.initialize(httpsServer);
