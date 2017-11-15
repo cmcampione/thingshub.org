0998d8dbf7534309db52487614ebee02f00e47f6
Quadrophenia <info@thingshub.org>
2017-11-15 13:33:54 +0100

    Realtime support for sdk

--- sdk/dist/connector.d.ts
+++ sdk/dist/connector.d.ts
@@ -5,15 +5,22 @@ export declare const enum ConnectionState {
     Disconnected = 4,
 }
 export interface StateChanged {
-    oldState: number;
-    newState: number;
+    oldState: ConnectionState;
+    newState: ConnectionState;
 }
-export interface Connector {
+export declare class Connector {
+    private authHook;
+    private stateChangedHook;
+    private reconnectedHook;
+    private subscribeSuccessHook;
+    private subscribeFailHook;
     subscribe(): void;
     unsubscribe(): void;
     setHook(eventName: string, hook: (...msg: any[]) => void): void;
     remHook(eventName: any, hook: (...msg: any[]) => void): void;
+    constructor(url: string, authHook: () => void, stateChangedHook: (change: StateChanged) => void, reconnectedHook: () => void, subscribeSuccessHook: () => void, subscribeFailHook: () => void);
 }
-export declare class Dummy {
-    dummy(): ConnectionState;
+export declare class SocketIOConnector extends Connector {
+    private socket;
+    constructor(url: string, authHook: () => void, stateChangedHook: (change: StateChanged) => void, reconnectedHook: () => void, subscribeSuccessHook: () => void, subscribeFailHook: () => void);
 }
--- sdk/src/connector.ts
+++ sdk/src/connector.ts
@@ -1,3 +1,5 @@
+import * as socketIo from 'socket.io-client';
+
 export const enum ConnectionState {
     Connecting = 0,
     Connected = 1,
@@ -6,21 +8,52 @@ export const enum ConnectionState {
 }
 
 export interface StateChanged {
-    oldState: number;
-    newState: number;
+    oldState: ConnectionState;
+    newState: ConnectionState;
 }
 
-export interface Connector {
+export class Connector {
+    
+    private authHook : () => void = null;
 
-    subscribe() : void;
-    unsubscribe() : void;
+    private stateChangedHook        : (change: StateChanged) => void = null;
+    private reconnectedHook         : () => void = null;
+    private subscribeSuccessHook    : () => void = null;
+    private subscribeFailHook       : () => void = null;
 
-    setHook(eventName : string, hook : (...msg: any[]) => void) : void;
-    remHook(eventName : any, hook : (...msg: any[]) => void) : void;
+    public subscribe() : void {}
+    public unsubscribe() : void {}
+
+    public setHook(eventName : string, hook : (...msg: any[]) => void) : void {}
+    public remHook(eventName : any, hook : (...msg: any[]) => void) : void {}
+
+    constructor(url : string,
+        authHook : () => void,
+        stateChangedHook : (change: StateChanged) => void, 
+        reconnectedHook : () => void,
+        subscribeSuccessHook : () => void, subscribeFailHook : () => void) {
+
+            this.authHook = authHook;
+            this.stateChangedHook = stateChangedHook;
+            this.reconnectedHook = reconnectedHook;
+            this.subscribeSuccessHook = subscribeSuccessHook;
+            this.subscribeFailHook = subscribeFailHook;
+    }
 }
 
-export class Dummy {
-    dummy() {
-        return ConnectionState.Connected;
+export class SocketIOConnector extends Connector
+{
+    private socket : SocketIOClient.Socket = null;
+    
+    constructor(url : string,
+        authHook : () => void,
+        stateChangedHook : (change: StateChanged) => void, 
+        reconnectedHook : () => void,
+        subscribeSuccessHook : () => void, subscribeFailHook : () => void) {
+            super(url,
+                authHook,
+                stateChangedHook, 
+                reconnectedHook,
+                subscribeSuccessHook, subscribeFailHook);
     }
 }
