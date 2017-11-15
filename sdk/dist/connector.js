f5da3917812be3e9a634322514666b430b011e9d
Carmelo Campione <cmcampione@gmail.com>
2017-11-15 13:33:54 +0100

    Realtime support for sdk

--- sdk/dist/connector.js
+++ sdk/dist/connector.js
@@ -1,7 +1,7 @@
 System.register([], function (exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
-    var ConnectionState, Dummy;
+    var ConnectionState, Connector, SocketIOConnector;
     return {
         setters: [],
         execute: function () {
@@ -12,12 +12,32 @@ System.register([], function (exports_1, context_1) {
                 ConnectionState[ConnectionState["Disconnected"] = 4] = "Disconnected";
             })(ConnectionState || (ConnectionState = {}));
             exports_1("ConnectionState", ConnectionState);
-            Dummy = class Dummy {
-                dummy() {
-                    return 1 /* Connected */;
+            Connector = class Connector {
+                constructor(url, authHook, stateChangedHook, reconnectedHook, subscribeSuccessHook, subscribeFailHook) {
+                    this.authHook = null;
+                    this.stateChangedHook = null;
+                    this.reconnectedHook = null;
+                    this.subscribeSuccessHook = null;
+                    this.subscribeFailHook = null;
+                    this.authHook = authHook;
+                    this.stateChangedHook = stateChangedHook;
+                    this.reconnectedHook = reconnectedHook;
+                    this.subscribeSuccessHook = subscribeSuccessHook;
+                    this.subscribeFailHook = subscribeFailHook;
+                }
+                subscribe() { }
+                unsubscribe() { }
+                setHook(eventName, hook) { }
+                remHook(eventName, hook) { }
+            };
+            exports_1("Connector", Connector);
+            SocketIOConnector = class SocketIOConnector extends Connector {
+                constructor(url, authHook, stateChangedHook, reconnectedHook, subscribeSuccessHook, subscribeFailHook) {
+                    super(url, authHook, stateChangedHook, reconnectedHook, subscribeSuccessHook, subscribeFailHook);
+                    this.socket = null;
                 }
             };
-            exports_1("Dummy", Dummy);
+            exports_1("SocketIOConnector", SocketIOConnector);
         }
     };
 });
