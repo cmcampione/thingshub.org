0cfdec43058a423456c32011872eb9bc1a9b01f8
Quadrophenia <info@thingshub.org>
2017-11-16 19:06:06 +0100

    Webpack improvement

--- sdk/webpack.config.js
+++ sdk/webpack.config.js
@@ -7,6 +7,9 @@ module.exports = {
 		filename: "thingshub-client-js.js",
 		library: "ThingsHubClient"
 	},
+	externals:{
+		"socket.io-client":"io"
+	},
 	resolve: {
 		// Add `.ts` and `.tsx` as a resolvable extension.
 		extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
