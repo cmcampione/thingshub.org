a62114fe53b74085430237525eae4c9f1ec79f18
Carmelo Campione <ccampione@xeniaprogetti.it>
2017-11-17 11:54:38 +0100

    First api call and realtime feedback

--- sdk/dist/connector.d.ts
+++ sdk/dist/connector.d.ts
@@ -6,8 +6,9 @@ export declare const enum ConnectionStates {
 }
 export declare class Connector {
     protected connectionStatus: ConnectionStates;
-    protected authHook: () => void;
     protected url: string;
+    protected authHook: () => void;
+    protected errorHook: (error: any) => void;
     protected stateChangedHook: (newState: ConnectionStates) => void;
     protected subscribeFailHook: () => void;
     protected on_connectionStatusChange(newState: ConnectionStates): void;
@@ -15,15 +16,18 @@ export declare class Connector {
     unsubscribe(): void;
     setHook(eventName: string, hook: (...msg: any[]) => void): void;
     remHook(eventName: any, hook: (...msg: any[]) => void): void;
-    constructor(url: string, authHook: () => void, stateChangedHook: (change: ConnectionStates) => void, subscribeFailHook: () => void);
+    constructor(url: string, authHook: () => void, errorHook: (error) => void, stateChangedHook: (change: ConnectionStates) => void, subscribeFailHook: () => void);
+    api(): Promise<any | any>;
 }
 export declare class SocketIOConnector extends Connector {
     private socket;
-    constructor(url: string, authHook: () => void, stateChangedHook: (change: ConnectionStates) => void, subscribeFailHook: () => void);
+    constructor(url: string, authHook: () => void, errorHook: (error) => void, stateChangedHook: (change: ConnectionStates) => void, subscribeFailHook: () => void);
     private on_error(error);
     private on_connect_error(error);
     private on_connect();
     private on_disconnect(reason);
     subscribe(): void;
     unsubscribe(): void;
+    setHook(eventName: string, hook: (...msg: any[]) => void): void;
+    remHook(eventName: any, hook: (...msg: any[]) => void): void;
 }
