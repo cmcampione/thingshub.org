c06ec1051d26b13cfbb91eb2e9187f6b15a82197
unknown <cmcampione@gmail.com>
2017-11-19 18:24:12 +0100

    Things Manager start

---
+++ server/src/models/Thing.js
@@ -0,0 +1,60 @@
+"use strict";
+
+const mongoose = require("mongoose");
+
+const sharedConst = require("../sharedConst");
+
+const thingSchema = mongoose.Schema({
+	_id                   : mongoose.Schema.Types.ObjectId,
+	creationDate          : Date,
+	name                  : String,
+	kind                  : String,
+	description           : String,
+	value                 : String,
+
+	deletedStatus         : { type: Number, default : sharedConst.ThingDeletedStates.Ok },
+	deletedDate       	  : Date,
+
+	publicReadClaims      : { type: Number, default : sharedConst.ThingUserReadClaims.NoClaim },
+	publicChangeClaims    : { type: Number, default : sharedConst.ThingUserChangeClaims.NoClaim },
+	everyoneReadClaims    : { type: Number, default : sharedConst.ThingUserReadClaims.NoClaim },
+	everyoneChangeClaims  : { type: Number, default : sharedConst.ThingUserChangeClaims.NoClaim },
+
+	userRights : [{
+		userId            : mongoose.Schema.Types.ObjectId,
+		username          : String,
+
+		userRole          : { type: Number, default : sharedConst.ThingUserRole.NoMatter },
+		userStatus        : { type: Number, default : sharedConst.ThingUserStates.NoMatter },
+		thingVisibility   : { type: Number, default : sharedConst.ThingUserVisibility.Visible },
+
+		userReadClaims    : { type: Number, default : sharedConst.ThingUserReadClaims.NoClaim },
+		userChangeClaims  : { type: Number, default : sharedConst.ThingUserChangeClaims.NoClaim },
+
+		shortPin          : Number
+	}],
+  
+	parentsThingsIds : [{
+		userId : mongoose.Schema.Types.ObjectId,
+		parentThingId : Number
+	}],
+  
+	childrenThings: [{
+		userId : mongoose.Schema.Types.ObjectId,
+		childThingId : mongoose.Schema.Types.ObjectId,
+		pos : Number
+	}]
+});
+
+const Thing = mongoose.model("Thing", thingSchema);
+
+/**
+ * Returns a thing if it finds one, otherwise returns null if a user is not found.
+ * @param   {String}   _id - The unique id of the user to find
+ * @returns {Promise} resolved user if found, otherwise resolves undefined
+ */
+exports.findThingById = _id => Thing.findById(_id).exec();
+
+exports.save = thing => thing.save(thing);
+
+exports.Thing = Thing;
---
+++ server/src/sharedConst.js
@@ -0,0 +1,159 @@
+"use strict";
+
+const ThingDeletedStates =
+{
+	NoMatter  : 0,  // Interna state. Do not useful for external usage
+	Ok        : 1,
+	Deleted   : 2,	
+};
+exports.ThingDeletedStates = ThingDeletedStates;
+
+exports.validateThingDeletedStatus = function validateThingDeletedStatus(deletedStatus) {
+	switch(deletedStatus) {
+	case ThingDeletedStates.NoMatter:
+	case ThingDeletedStates.Ok:
+	case ThingDeletedStates.Deleted:
+		return true;
+	}
+	return false;
+};
+
+// The User can not have more Roles in the same time
+const ThingUserRole =
+{
+	NoMatter      : 0,  // Internal state. Do not use for external use like filter
+	Administrator : 1,
+	User          : 2
+};
+exports.ThingUserRole = ThingUserRole;
+
+exports.validateThingUserRole = function validateThingUserRole(userRole) {
+	switch(userRole) {
+	case ThingUserRole.NoMatter:
+	case ThingUserRole.Administrator:
+	case ThingUserRole.User:
+		return true;
+	}
+	return false;
+};
+
+// The User can not have more Status in the same time
+const ThingUserStates = {
+	NoMatter    : 0,  // Internal state. Do not use for external use like filter
+	Ok          : 1,
+	WaitForAuth : 2,
+	Deleted     : 4
+};
+exports.ThingUserStates = ThingUserStates;
+
+exports.validateThingUserStatus = function validateThingUserStatus(userStatus) {
+	switch(userStatus) {
+	case ThingUserStates.NoMatter:
+	case ThingUserStates.Ok:
+	case ThingUserStates.WaitForAuth:
+	case ThingUserStates.Deleted:
+		return true;
+	}
+	return false;
+};
+
+const ThingUserVisibility = {
+	NoVisibility  : 0,  // Internal state. Do not use for external use like filter
+	Visible       : 1,
+	Hidden        : 2,
+};
+exports.ThingUserVisibility = ThingUserVisibility;
+
+exports.validateThingUserStatus = function validateThingUserStatus(visibility) {
+	switch(visibility) {
+	case ThingUserVisibility.NoVisibility:
+	case ThingUserVisibility.Visible:
+	case ThingUserVisibility.Hidden:
+		return true;
+	}
+	return false;
+};
+
+// Do not have validation function since are bitwise values
+exports.ThingUserReadClaims =
+{
+	NoClaim: 0,
+
+	CanReadThingUserChangeClaims: 1,
+
+	CanReadCreationDate: 2,
+	CanReadName: 4,
+	CanReadDescription: 8,
+	CanReadKind: 16,
+	CanReadValue: 32,
+	CanReadDeletedStatus: 64,
+	CanReadThingUserRights: 128,
+	CanReadThingUserRole: 256,
+	CanReadThingUserStatus: 512,
+	CanReadThingUserReadClaims: 1024,
+	// CanReadThingUserChangeClaims : 1,
+	CanReadPublicReadClaims: 2048,
+	CanReadPublicChangeClaims: 4096,
+	CanReadEveryoneReadClaims: 8192,
+	CanReadEveryoneChangeClaims: 16384,
+
+	// ShortCut
+
+	// INFO: Max int value 0x7FFFFFFF;//2147483647 - 32 bit with sign. Javascript bit manipulation limit
+	AllClaims: this.CanReadThingUserChangeClaims |
+  this.CanReadCreationDate | this.CanReadName | this.CanReadDescription |
+  this.CanReadKind | this.CanReadValue | this.CanReadDeletedStatus |
+  this.CanReadThingUserRights | this.CanReadThingUserRole |
+  this.CanReadThingUserStatus | this.CanReadThingUserReadClaims | this.CanReadPublicReadClaims |
+  this.CanReadPublicChangeClaims | this.CanReadEveryoneReadClaims | this.CanReadEveryoneChangeClaims,
+};
+
+// Do not have validation function since are bitwise values
+exports.ThingUserChangeClaims =
+{
+	NoClaim: 0,
+
+	CanDeleteThing: 1,
+	CanChangeName: 2,
+	CanChangeDescription: 4,
+	CanChangeKind: 8,
+	CanChangeValue: 16,
+	CanChangeDeletedStatus: 32,
+
+	CanAddThingUserRights: 64,
+	CanDeleteThingUserRights: 128,
+
+	CanChangeThingUserRole: 256,
+	CanChangeThingUserStatus: 512,
+
+	CanChangeThingUserReadClaims: 1024,
+	CanChangeThingUserChangeClaims: 2048,
+
+	CanChangePublicReadClaims: 4096,
+	CanChangePublicChangeClaims: 8192,
+	CanChangeEveryoneReadClaims: 16384,
+	CanChangeEveryoneChangeClaims: 32768,
+
+	CanAddChildrenThing: 65536,
+	CanRemoveChildrenThing: 131072,
+
+	// In beta test
+	CanOtherUsersChangeMyThingPos: 262144,
+
+	// ShortCut
+
+	// INFO: Max int value 0x7FFFFFFF;//2147483647 - 32 bit with sign. Javascript bit manipulation limit
+	AllClaims: this.CanDeleteThing | this.CanChangeName | this.CanChangeDescription |
+  this.CanChangeKind | this.CanChangeValue | this.CanChangeDeletedStatus |
+  this.CanAddThingUserRights | this.CanDeleteThingUserRights | this.CanChangeThingUserRole |
+  this.CanChangeThingUserStatus | this.CanChangeThingUserReadClaims | this.CanChangeThingUserChangeClaims |
+  this.CanChangePublicReadClaims | this.CanChangePublicChangeClaims | this.CanChangeEveryoneReadClaims | this.CanChangeEveryoneChangeClaims |
+  this.CanAddChildrenThing | this.CanRemoveChildrenThing,
+};
+
+exports.ThingKind = {
+	genericId : "1",
+	genericTxt : "the bees are laborious"
+};
+
+
