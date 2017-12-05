938a2909ca1daaeb677fe2c7916ff405eb607e7f
Carmelo Campione <cmcampione@gmail.com>
2017-12-05 18:51:06 +0100

    Start with TrackerApp

---
+++ clients/TrackerApp/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+
+import { OnsenModule } from 'ngx-onsenui';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    OnsenModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA,
+  ],
+})
+export class AppModule { }
