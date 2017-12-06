ab53332d24f071207004f1a2940ffb372316572e
Carmelo Campione <ccampione@xeniaprogetti.it>
2017-12-06 19:07:22 +0100

    Fix dist

--- sdk/dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-export * from "../../common/src/dtos";
-export * from "../../common/src/constants";
-export * from "./connector";
-export * from "./helpers";
-export * from "./endPointAddress";
-export * from "./thing";
-export * from "./thingsDataContext";
