590e33def40f3fb07f3c19c1d8b45b61cc179ba0
unknown <cmcampione@gmail.com>
2017-12-08 21:41:34 +0100

    Declaration file work in progress

--- sdk/webpack.config.js
+++ sdk/webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
 	resolve: {
 		// Add `.ts` and `.tsx` as a resolvable extension.
 		extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
-	},
+	},	
 	module: {
 		loaders: [
 			// all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
