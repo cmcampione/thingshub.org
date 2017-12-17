40ba70ca7ea80083dfe4f28e37188b51308ba954
unknown <cmcampione@gmail.com>
2017-12-17 21:46:15 +0100

    Fix webpack config

--- sdk/webpack.config.js
+++ sdk/webpack.config.js
@@ -12,13 +12,19 @@ module.exports = {
 		libraryTarget: "umd"
 	},
 	externals:{
-		"socket.io-client": "socket.io-client",
+		"socket.io-client": { 
+			amd: 'socket.io-client', 
+			global: 'io', // TODO: Can be unuseful?
+			root: 'io',
+			commonjs: 'socket.io-client', 
+			commonjs2: 'socket.io-client' 
+		},
 		"axios" : "axios"
 	},
 	resolve: {
 		// Add `.ts` and `.tsx` as a resolvable extension.
 		extensions: [".webpack.js", ".web.js", ".js", ".ts", ".tsx"]
-	},	
+	},
 	module: {
 		loaders: [
 			// all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
