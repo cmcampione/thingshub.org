ce85ff8ba63322ccdc926ca5b7f76b4b84082e18
unknown <cmcampione@gmail.com>
2017-12-19 19:44:46 +0100

    Start LocalStrategy

--- server/src/server.js
+++ server/src/server.js
@@ -9,6 +9,7 @@ const express 			= require("express");
 const expressValidator 	= require("express-validator");
 const bodyParser 		= require("body-parser");
 const passport 			= require("passport");
+const LocalStrategy 	= require('passport-local').Strategy;
 const LocalApiStrategy 	= require("passport-localapikey-update").Strategy;
 const mongoose  		= require("mongoose");
 const cors 				= require("cors");
@@ -58,11 +59,27 @@ app.use(expressValidator());
 
 // Passport setup
 
+/* 
+passport.use(new LocalStrategy(
+	function(username, password, done) {
+	  User.findOne({ username: username }, function (err, user) {
+		if (err) { return done(err); }
+		if (!user) {
+		  return done(null, false, { message: 'Incorrect username.' });
+		}
+		if (!user.validPassword(password)) {
+		  return done(null, false, { message: 'Incorrect password.' });
+		}
+		return done(null, user);
+	  });
+	}
+  )); 
+  */
+
 var localApiStrategyOptions = { 
 	apiKeyField: process.env.APIKEY_NAME,
 	apiKeyHeader: process.env.APIKEY_NAME
 };
-
 passport.use(new LocalApiStrategy(localApiStrategyOptions,
 	function(apikey, done) {
 		// asynchronous verification, for effect...
