4f7318215ba28ae9bcd8e799dc1471bf09acea52
unknown <cmcampione@gmail.com>
2018-01-01 20:47:33 +0100

    Work in progress

--- server/src/utils.js
+++ server/src/utils.js
@@ -67,10 +67,9 @@ const publicKey = fs.readFileSync(path.join(__dirname, "../certs/certificate.pem
  * @param  {String} sub - The subject or identity of the token.
  * @return {String} The JWT Token
  */
-exports.createToken = ({ exp = 3600, sub = "", mak = "", name = "" } = {}) => {
+exports.createToken = ({ exp = 3600, sub = "", name = "" } = {}) => {
   const token = jwt.sign({
 	jti : uuid(),
-	mak,
 	sub,
 	name,
     exp : Math.floor(Date.now() / 1000) + exp,
