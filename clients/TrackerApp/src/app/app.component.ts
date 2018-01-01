186f261dc2de0a49478cbee86c89995fcaeca148
unknown <cmcampione@gmail.com>
2018-01-01 21:25:35 +0100

    Work in progress

--- clients/TrackerApp/src/app/app.component.ts
+++ clients/TrackerApp/src/app/app.component.ts
@@ -2,19 +2,6 @@ import { Component } from '@angular/core';
 import { AccountService } from './account.service';
 import axios from "axios";
 
- // Add a response interceptor
- axios.interceptors.response.use(function (response) {
-  // Do something with response data
-  return response;
-}, (err) => {
-  if (err.response.status === 401) {
-
-   
-  }
-
-  return Promise.reject(err);
-});
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -27,6 +14,16 @@ export class AppComponent {
     status: this.accountService.isLoggedIn
   };
 
-  constructor(private accountService: AccountService) {    
+  constructor(private accountService: AccountService) {
+    // Add a response interceptor
+    axios.interceptors.response.use(response => {
+      // Do something with response data
+      return response;
+    }, err => {
+      if (err.response.status === 401) {
+        this.isLoggedIn.status = false;
+      }
+      return Promise.reject(err);
+    });
   }
 }
