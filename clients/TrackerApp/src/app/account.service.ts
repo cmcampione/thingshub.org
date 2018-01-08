32edef042f4ad947e26a3dceebbd9d6b2c0fc16b
Carmelo Campione <cmcampione@gmail.com>
2018-01-08 13:47:18 +0100

    Work in progress

--- clients/TrackerApp/src/app/account.service.ts
+++ clients/TrackerApp/src/app/account.service.ts
@@ -6,37 +6,6 @@ import { endPointAddress } from './utils';
 @Injectable()
 export class AccountService {
 
-  public getSecurityHeader = () => {
-    return { Authorization: 'Bearer ' + this.accountManager.accessToken};
-  }
-  private refreshToken = (): Promise<any> => {
-
-    // Show login Component
-    this._isLoggedIn.next(null);
-
-    return new Promise((resolve, reject) => {
-      const subscription = this.isLoggedIn$.subscribe((accountUserData: AccountUserData) => {
-        subscription.unsubscribe();
-        if (accountUserData != null) {
-          resolve(accountUserData);
-          return accountUserData;
-        }
-        // With accountUserData == null assert UserId changed without properly logout
-        const err = new Error("User is changed without appropriate logout action");
-        reject(err);
-        return err;
-      });
-    });
-  }
-
-  private actionControl : AccountActionControl = {
-    getSecurityHeader : this.getSecurityHeader,
-    refreshToken: this.refreshToken,
-    resetApp: () => {      
-      console.log("resetApp");
-    }
-  };
-
   private accountDataContext: AccountDataContext;
   private accountManager: AccountManager;
 
@@ -44,6 +13,33 @@ export class AccountService {
 
   private _isLoggedIn: Subject<AccountUserData> = new Subject<AccountUserData>();
   public isLoggedIn$ = this._isLoggedIn.asObservable();
+
+  private actionControl: AccountActionControl = {
+    getSecurityHeader : () => {
+      return { Authorization: 'Bearer ' + this.accountManager.accessToken};
+    },
+    refreshToken: (): Promise<any> => {
+
+      // Show login Component
+      this._isLoggedIn.next(null);
+      return new Promise((resolve, reject) => {
+        const subscription = this.isLoggedIn$.subscribe((accountUserData: AccountUserData) => {
+          subscription.unsubscribe();
+          if (accountUserData != null) {
+            resolve(accountUserData);
+            return accountUserData;
+          }
+          // With accountUserData == null assert UserId changed without properly logout
+          const err = new Error('User is changed without appropriate logout action');
+          reject(err);
+          return err;
+        });
+      });
+    },
+    resetApp: () => {
+      console.log('resetApp');
+    }
+  };
 
   constructor() {
 
@@ -61,11 +57,11 @@ export class AccountService {
       this.userId = loginData.id;
     }
     // The user is changed without properly logout
-    if (this.userId != loginData.id) {
+    if (this.userId !== loginData.id) {
       this.userId = null;
       this.accountManager.resetLoginData();
       this._isLoggedIn.next(null);
-      throw new Error("User is changed without appropriate logout action");
+      throw new Error('User is changed without appropriate logout action');
     }
     this._isLoggedIn.next(loginData);
     return loginData;
