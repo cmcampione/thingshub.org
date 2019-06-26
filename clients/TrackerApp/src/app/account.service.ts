d71b854cddcb77f793ed90b37a20d9d6670cf3f7
Quadrophenia <info@thingshub.org>
2019-06-26 17:44:39 +0200

    update server packages

--- clients/TrackerApp/src/app/account.service.ts
+++ clients/TrackerApp/src/app/account.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs/Subject';
+import { Subject } from 'rxjs';
 import { AccountDataContext, AccountUserData, AccountActionControl, AccountManager } from 'thingshub-js-sdk';
 import { endPointAddress } from './utils';
 
