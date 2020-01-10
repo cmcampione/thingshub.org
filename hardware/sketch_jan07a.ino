de7b90be68f5c2cbc28f5a2ff62947080c32f6ea
unknown <cmcampione@gmail.com>
2020-01-10 08:38:03 +0100

    hardware

---
+++ hardware/sketch_jan07a.ino
@@ -0,0 +1,43 @@
+#include "WiFi.h"
+
+// WiFi
+const char* wifi_ssid      = "Infostrada-702DC4";
+const char* wifi_password  = "HmqVyUeNQL";
+
+const unsigned long check_wifi_interval = 30000;
+unsigned long       check_wifi          = 0;
+boolean             wifi_reconnection   = false;
+
+int ledPin = 2;
+void setup()
+{
+  Serial.begin(115200);
+
+  // WiFi setup  
+  while (WiFi.status() != WL_CONNECTED) {
+    WiFi.begin(wifi_ssid, wifi_password);
+    delay(500);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to the WiFi");
+
+  //
+  pinMode(ledPin, OUTPUT);
+}
+void loop()
+{
+ // if wifi is down, try reconnecting every 30 seconds
+  if (WiFi.status() != WL_CONNECTED) {
+    wifi_reconnection = true;
+    if (millis() >= check_wifi) {      
+      WiFi.disconnect();
+      WiFi.begin(wifi_ssid, wifi_password);      
+      check_wifi = millis() + check_wifi_interval;
+      Serial.println("Reconnecting to WiFi...");
+    }
+  }
+  if ((WiFi.status() == WL_CONNECTED) && (wifi_reconnection == true)) {
+    wifi_reconnection = false;
+    Serial.println("Reconnected to WiFi");
+  }
+}
