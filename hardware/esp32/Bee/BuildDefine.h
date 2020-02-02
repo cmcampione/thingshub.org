20ff0cb85b76a2cdf4d8ceefaf7c8cd094014e63
unknown <cmcampione@gmail.com>
2020-02-02 19:59:42 +0100

    Some fix

--- hardware/esp32/Bee/BuildDefine.h
+++ hardware/esp32/Bee/BuildDefine.h
@@ -1,7 +1,20 @@
 #ifndef BuildDefine_h
 #define BuildDefine_h
 
-#define WIFISETUPEXTERNAL
+#define DEBUG
+#ifdef DEBUG    //Macros are usually in all capital letters.
+  #define DPRINT(...)    Serial.print(__VA_ARGS__)     //DPRINT is a macro, debug print
+  #define DPRINTF(...)    Serial.printf(__VA_ARGS__)     //DPRINT is a macro, debug print
+  #define DPRINTLN(...)  Serial.println(__VA_ARGS__)   //DPRINTLN is a macro, debug print with new line
+#else
+  #define DPRINT(...)     //now defines a blank line
+  #define DPRINTF(...)    //now defines a blank line
+  #define DPRINTLN(...)   //now defines a blank line
+#endif
+
 //#define DEBUG_BEESTATUS
+#define DEBUG_RCSENSORSMANAGER
+
+#define WIFISETUPEXTERNAL
 
 #endif
