5b6851d9fc436fa118803a3fa19c776954d6f0ef
unknown <cmcampione@gmail.com>
2020-02-09 12:17:22 +0100

    Fix realtimeconnector

--- clients/TrackerApp/src/app/real-time-connector.service.ts
+++ clients/TrackerApp/src/app/real-time-connector.service.ts
@@ -2,11 +2,18 @@ import { Injectable } from '@angular/core';
 import * as thingshub from 'thingshub-js-sdk';
 import { endPointAddress } from './utils';
 import { AccountService } from './account.service';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RealTimeConnectorService {
+
+  public readonly connectionStatus = new Subject<thingshub.RealtimeConnectionStates>();
+
+  private readonly onStateChanged = (change: thingshub.RealtimeConnectionStates): void => {
+    this.connectionStatus.next(change);
+  };
 
   private onError(error: any) {
     console.log(error);
@@ -14,15 +21,12 @@ export class RealTimeConnectorService {
   private onConnectError(error: any) {
     console.log(error);
   }
-  private onStateChanged(change: any) {
-    console.log(change);
-  }
-  public realTimeConnectorRaw = new thingshub.SocketIORealtimeConnector(
+
+  public readonly realTimeConnectorRaw = new thingshub.SocketIORealtimeConnector(
     endPointAddress.server,
     this.accountService.getSecurityToken,
     this.onError, this.onConnectError, this.onStateChanged);
 
-  constructor(private accountService: AccountService) {
-    this.realTimeConnectorRaw.subscribe();
-   }
+  constructor(private accountService: AccountService) {    
+  }
 }
