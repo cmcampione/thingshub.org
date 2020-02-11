bb12b500fb1c55ed14358ec723c744cd5380689f
unknown <cmcampione@gmail.com>
2020-02-11 21:31:40 +0100

    Start thingsManager Test

--- clients/TrackerApp/src/app/things/things.component.ts
+++ clients/TrackerApp/src/app/things/things.component.ts
@@ -1,77 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import * as thingshub from 'thingshub-js-sdk';
-import { endPointAddress } from '../utils';
 import { AccountService } from '../account.service';
+import { ThingsManagerService, THING_KIND } from '../things-manager.service';
+
 @Component({
   selector: 'app-things',
   templateUrl: './things.component.html',
-  styleUrls: ['./things.component.css']
+  styleUrls: ['./things.component.css'],
+  providers: [ThingsManagerService, 
+    { provide: THING_KIND, useValue: 'first thing' }    
+  ]
 })
-export class ThingsComponent implements OnInit {
+export class ThingsComponent implements OnInit, OnDestroy {
 
-  private thingsDataContext: thingshub.ThingsDataContext;
-  public things: thingshub.Thing[] = [];
-  public things1: thingshub.Thing[] = [];
+  private canceler = new thingshub.HttpRequestCanceler();
+  public things: thingshub.Thing[];
 
-  constructor(private accountService: AccountService) {
-    this.thingsDataContext = new thingshub.ThingsDataContext(endPointAddress, accountService.getSecurityHeader);
+  constructor(private readonly accountService: AccountService,
+    private readonly thingsManager: ThingsManagerService) {
+      this.things = this.thingsManager.mainThing.children;
   }
 
-  ngOnInit() {
+  async ngOnInit() {        
+    await this.thingsManager.thingsManager.getMoreThings(this.canceler);
+  }
 
-    const thingsGetParams: thingshub.ThingsGetParams = {
-      parentThingId : null,
-      thingFilter : null,
-      valueFilter : null,
-      orderBy : '',
-      skip : 0,
-      top : 100
-    };
+  ngOnDestroy() {
 
-    this.thingsDataContext.getThings(thingsGetParams)
-    .then(things => {
-      for (let i = 0; i < things.things.length; i++) {
-      this.things.push(new thingshub.Thing(things.things[i]));
-    }});
-    console.log('things = ' + this.things);
-
-    this.thingsDataContext.getThings(thingsGetParams)
-    .then(things => {
-      for (let i = 0; i < things.things.length; i++) {
-        this.things1.push(new thingshub.Thing(things.things[i]));
-      }
-    });
-    console.log('things1 = ' + this.things1);
   }
 
   public async recall() {
-
-    const thingsGetParams: thingshub.ThingsGetParams = {
-      parentThingId : null,
-      thingFilter : null,
-      valueFilter : null,
-      orderBy : '',
-      skip : 0,
-      top : 100
+    try {
+      await this.thingsManager.thingsManager.getMoreThings(this.canceler);
+    }
+    catch(e) {
+      console.log(e);
     };
-
-    this.thingsDataContext.getThings(thingsGetParams)
-    .then(things => {
-      for (let i = 0; i < things.things.length; i++) {
-      this.things.push(new thingshub.Thing(things.things[i]));
-    }})
-    .catch(e => {
-      console.log(e);
-    });
-
-    this.thingsDataContext.getThings(thingsGetParams)
-    .then(things => {
-      for (let i = 0; i < things.things.length; i++) {
-        this.things1.push(new thingshub.Thing(things.things[i]));
-      }
-    })
-    .catch(e => {
-      console.log(e);
-    });
   }
 }
