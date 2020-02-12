a9001948c1a943b421d89a76a69ec366fe0aade2
Carmelo Campione <ccampione@xeniaprogetti.it>
2020-02-12 09:21:11 +0100

    typos

--- clients/PeraColta/src/app/things/things.component.ts
+++ clients/PeraColta/src/app/things/things.component.ts
@@ -1,14 +1,13 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import * as thingshub from 'thingshub-js-sdk';
-import { AccountService } from '../account.service';
 import { ThingsManagerService, THING_KIND } from '../things-manager.service';
 
 @Component({
   selector: 'app-things',
   templateUrl: './things.component.html',
   styleUrls: ['./things.component.css'],
-  providers: [ThingsManagerService, 
-    { provide: THING_KIND, useValue: 'first thing' }    
+  providers: [ThingsManagerService,
+    { provide: THING_KIND, useValue: 'first thing' }
   ]
 })
 export class ThingsComponent implements OnInit, OnDestroy {
@@ -16,12 +15,11 @@ export class ThingsComponent implements OnInit, OnDestroy {
   private canceler = new thingshub.HttpRequestCanceler();
   public things: thingshub.Thing[];
 
-  constructor(private readonly accountService: AccountService,
-    private readonly thingsManager: ThingsManagerService) {
+  constructor(private readonly thingsManager: ThingsManagerService) {
       this.things = this.thingsManager.mainThing.children;
   }
 
-  async ngOnInit() {        
+  async ngOnInit() {
     await this.thingsManager.thingsManager.getMoreThings(this.canceler);
   }
 
@@ -30,11 +28,10 @@ export class ThingsComponent implements OnInit, OnDestroy {
   }
 
   public async recall() {
-    try {
-      await this.thingsManager.thingsManager.getMoreThings(this.canceler);
-    }
-    catch(e) {
-      console.log(e);
-    };
+      try {
+        await this.thingsManager.thingsManager.getMoreThings(this.canceler);
+      } catch (e) {
+        console.log(e);
+      }
   }
 }
