faca0ff2ede3204421e32776ea916d25afca4fa3
Carmelo Campione <ccampione@xeniaprogetti.it>
2020-02-13 11:29:31 +0100

    Lab on angular services

--- clients/PeraColta/src/app/sensors.service.ts
+++ clients/PeraColta/src/app/sensors.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Sensor } from './sensor';
-import { ThingsManagerService, THING_KIND } from './things-manager.service';
+import { ThingsManagerService } from './things-manager.service';
 
 @Injectable({
   providedIn: 'root'
