6c4d5d888ae60e46e8815215e7bd25456a9ba212
unknown <cmcampione@gmail.com>
2020-02-16 19:44:58 +0100

    Implemented Logout

--- clients/PeraColta/src/app/app.component.ts
+++ clients/PeraColta/src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component, ViewChild, OnDestroy } from '@angular/core';
 import { SidePageComponent } from './side-page/side-page.component';
 import { ContentPageComponent } from './content-page/content-page.component';
 import { MenuService } from './menu.service';
+import { AccountService } from './account.service';
+import { AccountUserData } from 'thingshub-js-sdk';
 
 @Component({
   selector: 'app-root',
@@ -15,7 +17,13 @@ export class AppComponent implements OnDestroy {
 
   @ViewChild('splitter', {static: false}) splitter;
 
-  constructor(private menuService: MenuService) {
+  public isLoggedIn: boolean = false;
+  private readonly checkLogin = (accountUserData: AccountUserData) => {
+    this.isLoggedIn = accountUserData != null;
+  };
+  
+  constructor(private accountService: AccountService, private menuService: MenuService) {
+    this.accountService.isLoggedIn.subscribe(this.checkLogin);
     this.menuService.toggle.subscribe(() => this.splitter.nativeElement.side.open());
   }
 
--- clients/PeraColta/src/app/side-page/side-page.component.ts
+++ clients/PeraColta/src/app/side-page/side-page.component.ts
@@ -1,14 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../account.service';
+import { AccountUserData } from 'thingshub-js-sdk';
 
 @Component({
-  // tslint:disable-next-line:component-selector
   selector: 'ons-page',
   templateUrl: './side-page.component.html',
   styleUrls: ['./side-page.component.css']
 })
 export class SidePageComponent implements OnInit {
 
-  constructor() { }
+  public isLoggedIn: boolean = false;
+  private readonly checkLogin = (accountUserData: AccountUserData) => {
+    this.isLoggedIn = accountUserData != null;
+  };
+
+  constructor(private accountService: AccountService) {
+    this.accountService.isLoggedIn.subscribe(this.checkLogin);
+  }
 
   ngOnInit() {
   }
