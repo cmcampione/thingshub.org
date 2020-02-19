88fde17edcfbe7020007a154fe9d7639505e3477
Quadrophenia <info@thingshub.org>
2020-02-19 18:26:01 +0100

    Start with ionic component

--- clients/PeraColta/src/app/app.module.ts
+++ clients/PeraColta/src/app/app.module.ts
@@ -9,10 +9,27 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { AgmCoreModule } from '@agm/core';
+import { LoginComponent } from './login/login.component';
+import { MapComponent } from './map/map.component';
+import { ThingsComponent } from './things/things.component';
+import { SidePageComponent } from './side-page/side-page.component';
+import { ContentPageComponent } from './content-page/content-page.component';
+import { SensorsComponent } from './sensors/sensors.component';
+
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent,
+    LoginComponent,
+    MapComponent,
+    SensorsComponent,
+    ThingsComponent,
+    SidePageComponent,
+    ContentPageComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyD11pjYHyE0ekfygLBNJhvL1FgUp9-twkQ'
+    })],
   providers: [
     StatusBar,
     SplashScreen,
