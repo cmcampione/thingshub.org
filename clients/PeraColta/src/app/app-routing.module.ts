91cbadfe53facf703c52d4d7fbce3553c0a0f9a3
Carmelo Campione <cmcampione@gmail.com>
2020-02-19 18:26:01 +0100

    Start with ionic component

--- clients/PeraColta/src/app/app-routing.module.ts
+++ clients/PeraColta/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { ContentPageComponent} from '../app/content-page/content-page.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  //{ path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'home', component: ContentPageComponent},
 ];
 
 @NgModule({
