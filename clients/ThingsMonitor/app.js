d00c232ade347664f45238bc2412b087d8aa055c
unknown <cmcampione@gmail.com>
2020-02-22 21:22:51 +0100

    ThingsMonitor first connection

--- clients/ThingsMonitor/app.js
+++ clients/ThingsMonitor/app.js
@@ -1,32 +1,105 @@
+const readline = require('readline');
 const path = require("path");
 const dotenv = require("dotenv");
-const readline = require('readline');
 const thingshub = require('thingshub-js-sdk');
 
 // Env configuration
-
 const configPath = path.join(__dirname, "./", "thingsMonitor.env");
 dotenv.config({ path: configPath });
 
-var dummy = process.env.MAIN_API_KEY;
-
-let thingsForConfig = [];
-let thingsToMonitor = [
-  "f4c3c80b-d561-4a7b-80a5-f4805fdab9bb", // My Home
-];
-
-const EndPoint = {
-  server: "https://api.thingshub.org:3000",
-  api: "https://api.thingshub.org:3000/api"
+//
+var mainApiKey = process.env.MAIN_API_KEY;
+if (!mainApiKey) {
+  console.log("Main ApiKey not found in .env file");
+  process.exit();
 }
-const accountDataContext = new thingshub.AccountDataContext(EndPoint);
-// demo@thingshub.org apikey
-const accountManager = new thingshub.AccountManager("thingshub", accountDataContext, "491e94d9-9041-4e5e-b6cb-9dad91bbf63d");
+const endPoint = {
+  server: process.env.ENDPOINT_SERVER,
+  api: process.env.ENDPOINT_API
+}
+if (!endPoint.server || !endPoint.api) {
+  console.log("EndPoint data not found in .env file");
+  process.exit();
+}
+
+let configThingKind = process.env.CONFIG_THING_KIND
+if (!configThingKind) {
+  console.log("Config Thing Kind not found in .env file");
+  process.exit();
+}
+
+//
+const accountDataContext = new thingshub.AccountDataContext(endPoint);
+//
+const accountManager = new thingshub.AccountManager("thingshub", accountDataContext, mainApiKey);
+
+function onError(error) {
+  console.log(error);
+}
+function onConnectError(error) {
+  console.log(error);
+}
+
+function onStateChanged(change) {
+  console.log(change);
+}
+
+function onAPI(data) {
+  console.log(data);
+}
+
+function onUpdateThing(thingDTO) {
+  console.log(thingDTO);
+}
+function onUpdateThingValue(thingId, value, asCmd) {
+  console.log(value);
+}
+
+var realTimeConnector = new thingshub.SocketIORealtimeConnector(endPoint.server, accountManager.getSecurityToken, onError, onConnectError, onStateChanged);
+realTimeConnector.subscribe();
+realTimeConnector.setHook("api", onAPI);
+realTimeConnector.setHook("onUpdateThing", onUpdateThing);
+realTimeConnector.setHook("onUpdateThingValue", onUpdateThingValue);
+
+realTimeConnector.api()
+.then(function(res) {
+  console.log(res);
+})
+.catch(function(err) {
+  console.log(err);
+});
+
+// 
+
+var mainThingForConfig = new thingshub.Thing();
+var thingsManagerClaims = {
+
+  publicReadClaims : thingshub.ThingUserReadClaims.NoClaims,
+  publicChangeClaims: thingshub.ThingUserChangeClaims.NoClaims,
+
+  everyoneReadClaims: thingshub.ThingUserReadClaims.NoClaims,
+  everyoneChangeClaims: thingshub.ThingUserChangeClaims.NoClaims,
+
+  creatorUserReadClaims: thingshub.ThingUserReadClaims.AllClaims,
+  creatorUserChangeClaims: thingshub.ThingUserChangeClaims.AllClaims
+}
+var thingsDatacontext = new thingshub.ThingsDataContext(endPoint, accountManager.getSecurityHeader);
+var thingsManager = new thingshub.ThingsManager(mainThingForConfig, configThingKind, thingsManagerClaims, thingsDatacontext, realTimeConnector );
+var httpRequestCanceler = new thingshub.HttpRequestCanceler();
+
+thingsManager.getMoreThings(httpRequestCanceler)
+    .then((data) => {
+      console.log(mainThingForConfig);        
+    })
+    .catch((e) => {
+      console.log(e);
+    });
 
 readline.emitKeypressEvents(process.stdin);
 process.stdin.setRawMode(true);
 process.stdin.on('keypress', (str, key) => {
   if (key.ctrl && key.name === 'c') {
+    console.log('bye');
     process.exit();
   } else {
     console.log(`You pressed the "${str}" key`);
