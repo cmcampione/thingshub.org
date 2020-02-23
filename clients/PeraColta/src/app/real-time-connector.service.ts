32cb803e1db528ae1340cf0237530766b8ba4517
unknown <cmcampione@gmail.com>
2020-02-23 21:02:59 +0100

    Fix in ThingsMonitor

--- clients/PeraColta/src/app/real-time-connector.service.ts
+++ clients/PeraColta/src/app/real-time-connector.service.ts
@@ -9,27 +9,26 @@ import { Subject } from 'rxjs';
 })
 export class RealTimeConnectorService {
 
+  constructor(private accountService: AccountService) {
+  }
+
   public readonly connectionStatus = new Subject<thingshub.RealtimeConnectionStates>();
 
+   private readonly onError = (error: any) => {
+    console.log(error);
+  }
+  private readonly onConnectError = (error: any) => {
+    console.log(error);
+  }
   private readonly onStateChanged = (change: thingshub.RealtimeConnectionStates): void => {
     this.connectionStatus.next(change);
   };
 
-  // ToDo: To trasform like onStateChanged
-  private onError(error: any) {
-    console.log(error);
-  }
-  // ToDo: To trasform like onStateChanged
-  private onConnectError(error: any) {
-    console.log(error);
-  }
-
   // ToDo: Could be injected to chande connection type
+  // tslint:disable-next-line: member-ordering
   public readonly realTimeConnectorRaw = new thingshub.SocketIORealtimeConnector(
     endPointAddress.server,
     this.accountService.getSecurityToken,
     this.onError, this.onConnectError, this.onStateChanged);
 
-  constructor(private accountService: AccountService) {    
-  }
 }
