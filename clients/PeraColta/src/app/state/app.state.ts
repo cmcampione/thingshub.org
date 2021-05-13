f24487871b3ecff278d3aa937725310ecf4f3b03
Carmelo Campione <cmcampione@hotmail.com>
2021-05-13 10:05:01 +0200

    Start with send command

--- clients/PeraColta/src/app/state/app.state.ts
+++ clients/PeraColta/src/app/state/app.state.ts
@@ -1,6 +1,5 @@
 import { SensorValue } from '../sensors/sensor-value.model';
 import { SensorConfig } from '../sensors/sensor-config.model';
-
 export interface AppState {
     sensorsValue: ReadonlyArray<SensorValue>;
     sensorsConfig: ReadonlyArray<SensorConfig>;
