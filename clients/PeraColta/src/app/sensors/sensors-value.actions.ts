90c9f8d57bf4f911e7e18183b8cf71189606d2dd
Carmelo Campione <cmcampione@hotmail.com>
2021-05-19 18:15:12 +0200

    Action Reset App

--- clients/PeraColta/src/app/sensors/sensors-value.actions.ts
+++ clients/PeraColta/src/app/sensors/sensors-value.actions.ts
@@ -1,9 +1,10 @@
 import { createAction, props } from '@ngrx/store';
+import { RESET_APP_STATE } from '../app.actions';
 import { SensorValue } from '../sensors/sensor-value.model';
 
 export const GET_ALL_SENSORS_VALUE          = '[Sensors Value] Load Sensors Value';
 export const GET_ALL_SENSORS_VALUE_SUCCESS  = '[Sensors Value] Sensors Value Loaded Success';
-export const SET_SENSOR_VALUE            = '[Sensors Value] Select sensor value';
+export const SET_SENSOR_VALUE               = '[Sensors Value] Select sensor value';
 
 export const getAllSensorsValue = createAction(
     GET_ALL_SENSORS_VALUE
--- clients/PeraColta/src/app/state/app.state.ts
+++
@@ -1,6 +0,0 @@
-import { SensorValue } from '../sensors/sensor-value.model';
-import { SensorConfig } from '../sensors/sensor-config.model';
-export interface AppState {
-    sensorsValue: ReadonlyArray<SensorValue>;
-    sensorsConfig: ReadonlyArray<SensorConfig>;
-}
