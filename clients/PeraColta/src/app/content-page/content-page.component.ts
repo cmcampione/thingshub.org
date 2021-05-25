d30379bef361ad8983ec948362af22110ccbfd48
Carmelo Campione <cmcampione@hotmail.com>
2021-05-25 16:59:55 +0200

    Little code review

--- clients/PeraColta/src/app/content-page/content-page.component.ts
+++ clients/PeraColta/src/app/content-page/content-page.component.ts
@@ -3,6 +3,10 @@ import { AccountService } from '../account.service';
 import { RealTimeConnectorService } from '../real-time-connector.service';
 import * as thingshub from 'thingshub-js-sdk';
 import { Subscription } from 'rxjs';
+import { select, Store } from '@ngrx/store';
+import { selectSensorsCount as selectSensorsCount, selectSensors } from '../sensors/sensors.selectors';
+import { getAllSensorsConfig } from '../sensors/sensors-config.actions';
+import { getAllSensorsValue } from '../sensors/sensors-value.actions';
 
 @Component({
   selector: 'app-home',
@@ -24,13 +28,26 @@ export class ContentPageComponent implements OnInit, OnDestroy {
   // They are public for this: https://stackoverflow.com/questions/34574167/angular2-should-private-variables-be-accessible-in-the-template/34574732#34574732
   public connIconName = 'globe';
   public connIconColor = 'danger';
-  public isLoggedIn = false;
+  public isLoggedIn: boolean = false;
+
+  public sensorsCount$ = this.store.pipe(select(selectSensorsCount));
 
   private readonly checkLogin = (isLoggedIn: boolean) => {
     this.isLoggedIn = isLoggedIn;
-    if (this.isLoggedIn) {
+    if (this.isLoggedIn) {      
       this.realTimeConnector.realTimeConnectorRaw.subscribe();
+
+      // Reducers are pure functions in that they produce the same output for a given input.
+      // They are without side effects and handle each state transition synchronously.
+      // Each reducer function takes the latest Action dispatched, the current state,
+      // and determines whether to return a newly modified state or the original state.
+      // https://ngrx.io/guide/store/reducers
+
+      // Below methods are here because we need to know the number of sensors before SensorsComponent is displayed
+      this.store.dispatch(getAllSensorsConfig()); // It is syncronous as abose comment
+      this.store.dispatch(getAllSensorsValue());  // It is syncronous as abose comment
     } else {
+      // ToDo: Arrive before other components can remhook from realtime connector
       this.realTimeConnector.realTimeConnectorRaw.unsubscribe();
     }
   }
@@ -56,7 +73,8 @@ export class ContentPageComponent implements OnInit, OnDestroy {
     }
   };
 
-  constructor(private accountService: AccountService,
+  constructor(private readonly store: Store,
+    private accountService: AccountService,
     private realTimeConnector: RealTimeConnectorService) {
       this.subscriptionIsLoggedIn = this.accountService.isLoggedIn$.subscribe(this.checkLogin);
       this.subscriptionRealTimeConnector = this.realTimeConnector.connectionStatus$.subscribe({
@@ -69,6 +87,16 @@ export class ContentPageComponent implements OnInit, OnDestroy {
     // Info: Useful when a page refresh occurs and the access token is still valid
     if (this.isLoggedIn) {
       this.realTimeConnector.realTimeConnectorRaw.subscribe();
+
+      // Reducers are pure functions in that they produce the same output for a given input.
+      // They are without side effects and handle each state transition synchronously.
+      // Each reducer function takes the latest Action dispatched, the current state,
+      // and determines whether to return a newly modified state or the original state.
+      // https://ngrx.io/guide/store/reducers
+
+      // Below methods are here because we need to know the number of sensors before SensorsComponent is displayed
+      this.store.dispatch(getAllSensorsConfig()); // It is syncronous as abose comment
+      this.store.dispatch(getAllSensorsValue());  // It is syncronous as abose comment
     }
   }
 
