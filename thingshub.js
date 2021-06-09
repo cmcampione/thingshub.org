09dd1e8b5ade4ef4a4de26e6341e21008f81efe0
Carmelo Campione <cmcampione@hotmail.com>
2021-06-09 20:10:35 +0200

    New bee

--- thingshub.js
+++ thingshub.js
@@ -345,7 +345,9 @@ var $apply = GetIntrinsic('%Function.prototype.apply%');
 var $call = GetIntrinsic('%Function.prototype.call%');
 var $reflectApply = GetIntrinsic('%Reflect.apply%', true) || bind.call($call, $apply);
 
+var $gOPD = GetIntrinsic('%Object.getOwnPropertyDescriptor%', true);
 var $defineProperty = GetIntrinsic('%Object.defineProperty%', true);
+var $max = GetIntrinsic('%Math.max%');
 
 if ($defineProperty) {
 	try {
@@ -356,8 +358,20 @@ if ($defineProperty) {
 	}
 }
 
-module.exports = function callBind() {
-	return $reflectApply(bind, $call, arguments);
+module.exports = function callBind(originalFunction) {
+	var func = $reflectApply(bind, $call, arguments);
+	if ($gOPD && $defineProperty) {
+		var desc = $gOPD(func, 'length');
+		if (desc.configurable) {
+			// original length, plus the receiver, minus any additional arguments (after the receiver)
+			$defineProperty(
+				func,
+				'length',
+				{ value: 1 + $max(0, originalFunction.length - (arguments.length - 1)) }
+			);
+		}
+	}
+	return func;
 };
 
 var applyBind = function applyBind() {
