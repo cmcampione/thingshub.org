beb6e6a610f6ff1e75b652eced345e1b50e9095d
Carmelo Campione <cmcampione@hotmail.com>
2021-12-30 11:09:53 +0100

    Little code fix

--- clients/PeraColta/src/app/sensors1/sensor.model.ts
+++ clients/PeraColta/src/app/sensors1/sensor.model.ts
@@ -1,13 +1,8 @@
-import { SensorConfig, SensorKind, SensorKindType } from './sensor-config.model';
+import { SensorConfig } from './sensor-config.model';
 import { SensorValue } from './sensor-value.model';
 export interface Sensor {
   thingId: string,
   id: string,
-  // ToDo: To remove
-  name: string,
-  now: boolean,
-  millis: number,
-  value: number,
 
   sensorConfig: SensorConfig,
   sensorValue : SensorValue
