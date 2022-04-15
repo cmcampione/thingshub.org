f5c15656d1e3033ae8c02d2646f71b6c9659b37e
Carmelo Campione <cmcampione@hotmail.com>
2022-04-15 15:13:04 +0200

    Fix refresh

--- clients/PeraColta/src/app/sensors2/sensors/sensors.component.ts
+++ clients/PeraColta/src/app/sensors2/sensors/sensors.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, OnDestroy, Input } from '@angular/core';
+import { Sensor } from 'src/app/sensors2/sensor.model';
 import { ThingSensor } from '../thing-sensor.model';
 @Component({
   selector: 'app-sensors',
@@ -16,5 +17,10 @@ export class SensorsComponent implements OnInit, OnDestroy {
   }
   // Info: Called every time component is hidden
   ngOnDestroy() {
+  }
+
+  // https://netbasal.com/angular-2-improve-performance-with-trackby-cc147b5104e5
+  trackByFn(index, item: Sensor) {
+    return item.id;
   }
 }
